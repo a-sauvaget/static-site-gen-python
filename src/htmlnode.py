0382e6c691e507ba205792310810ce85271659d6
Alexandre Sauvaget <70508923+a-sauvaget@users.noreply.github.com>
2024-12-04 15:27:58 +0100

    ajout subclasse parentnode + tests unitaires

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,4 +1,5 @@
 class HTMLNode:
+    '''A node in an HTML document tree.'''
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
@@ -21,6 +22,7 @@ class HTMLNode:
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
 
 class LeafNode(HTMLNode):
+    '''A single HTML tag with no children.'''
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
 
@@ -31,3 +33,22 @@ class LeafNode(HTMLNode):
         elif self.tag is None:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    '''Handle nesting of HTML nodes insides one another.'''
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        '''Render the HTML tag of the node and it's children.'''
+        if self.tag is None:
+            raise ValueError("Invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("Invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
